8cde505bd9a3fa046cfae9357fc08e70fe1bd2e3
Juancho <linanjm90@gmail.com>
2019-02-24 22:49:52 -0600

    extrac the work item to a new component

---
+++ src/components/worksItem/workItem.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="portfolio-item col-xs-12 col-sm-4 col-md-3" v-bind:data-groups='item.groups'>
+   <div class="portfolio-bg">
+     <div class="portfolio">
+       <div class="tt-overlay"></div>
+       <div class="links">
+         <a class="image-link" href="assets/images/works/totalpunch.jpeg"><i class="fa fa-search-plus"></i></a>
+         <a v-bind:href="item.link"><i class="fa fa-link"></i></a>                          
+       </div><!-- /.links -->
+       <img v-bind:src="item.image" alt="image"> 
+       <div class="portfolio-info">
+         <h3>{{ item.title }}</h3>
+       </div><!-- /.portfolio-info -->
+     </div><!-- /.portfolio -->
+   </div><!-- /.portfolio-bg -->
+ </div><!-- /.portfolio-item -->
+</template>
+
+<script>
+  export default {
+      name: 'WorkItem',
+      props: {
+          item: Object
+      }
+  }
+</script>
+
